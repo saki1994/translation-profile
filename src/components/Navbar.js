cdc94389759406f5e2ed818fc95f306875292081
saki1994 <67071604+saki1994@users.noreply.github.com>
2021-11-25 12:32:51 +0100

    adjusting body @media

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,12 @@ import { List } from "@mui/material";
 
 const Navbar = () => {
   return (
-    <nav id="navbar" className="navbar nav-mobile">
-      <List className="nav-links">
-        <NavLink link="#home" text="Home" />
-        <NavLink link="#services" text="Services" />
-        <NavLink link="#testimony" text="Testimony" />
-        <NavLink link="#contact" text="Contact" />
-      </List>
-    </nav>
+    <List className="nav-links">
+      <NavLink link="#home" text="Home" />
+      <NavLink link="#services" text="Services" />
+      <NavLink link="#testimony" text="Testimony" />
+      <NavLink link="#contact" text="Contact" />
+    </List>
   );
 };
 
