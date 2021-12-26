03b23c852a7aff4aaa8dac609e69deb6689570f5
saki1994 <67071604+saki1994@users.noreply.github.com>
2021-12-26 19:11:00 +0100

    deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { LanguagesData } from "../Apps";
 import NavLink from "./NavLink.js";
 import { List } from "@mui/material";
